004e6d0aae77398478126016a4212a9e983aaa22
PeretsPasha <perets.pasha.pro@gmail.com>
2020-05-13 17:06:24 +0300

    New commit

---
+++ app/src/main/java/com/example/travelrecycler/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.travelrecycler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
