e1d14f984f1ce3ceff5e30198a1454e268630e9a
Iron <perets.pasha.pro@gmail.com>
2020-05-16 20:50:11 +0300

    Hello

--- app/src/main/java/com/example/travelrecycler/MainActivity.java
+++ app/src/main/java/com/example/travelrecycler/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Hi !!!!
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
